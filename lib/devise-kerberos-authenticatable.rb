d501cf8a1e9c78d65d2fc95e951bf70d49c77b4b
Paul Rowe-White <paulrowe-white@Walternate.local>
2014-01-29 20:59:43 +0000

    Moved route configuration

--- lib/devise-kerberos-authenticatable.rb
+++ lib/devise-kerberos-authenticatable.rb
@@ -4,12 +4,12 @@ $: << File.expand_path('..', __FILE__)
 
 require 'devise_kerberos_authenticatable/model'
 require 'devise_kerberos_authenticatable/strategy'
-require 'devise_kerberos_authenticatable/routes'
 require 'devise_kerberos_authenticatable/kerberos_adapter'
 
 Devise.add_module(
   :kerberos_authenticatable,
   :strategy                   => true,
   :model                      => 'devise_kerberos_authenticatable/model',
-  :route                      => true
+  :route                      => :session,
+  :controller                 => :sessions
 )
--- lib/devise_kerberos_authenticatable/routes.rb
+++
@@ -1,4 +0,0 @@
-ActionController::Routing::Mapper.class_eval do
-  protected
-    alias_method :devise_kerberos_authenticatable, :devise_session
-end
